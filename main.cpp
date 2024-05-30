3bdc4861bda9d9dd2b6573248c08d6a40802194f
Nikita Krenov <nikitabaram@gmail.com>
2024-05-30 20:29:58 +0300

    pr 2 try to tr prot

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
 int main() {
+    const int ROWS = 3;
+    const int COLS = 4;
+    int arr[ROWS][COLS];
+    int rowSums[ROWS] = { 0 };
+    int colSums[COLS] = { 0 };
+    int totalSum = 0;
+
+    cout << "Enter elements of the array:" << endl;
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            rowSums[i] += arr[i][j];
+            colSums[j] += arr[i][j];
+            totalSum += arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            cout << setw(4) << arr[i][j];
+        }
+        cout << " | " << rowSums[i] << endl;
+    }
+
+    cout << "  " << string(COLS * 4, '-') << endl;
+
+    for (int j = 0; j < COLS; ++j) {
+        cout << setw(4) << colSums[j];
+    }
+    cout << " | " << totalSum << endl;
 	
 	system("pause");
 	return 0;
